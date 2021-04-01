f1fd13712db2d5ba7d6477031f017572a85eb929
macricek <marko.chylik@gmail.com>
2021-04-02 00:12:15 +0200

    trackeeer

--- uloha1f.py
+++ uloha1f.py
@@ -13,14 +13,10 @@ def defineParams(hsvFrame, askedColor):
     sensitivity = 70
     white_lower = np.array([0, 0, 255 - sensitivity])
     white_upper = np.array([255, sensitivity, 255])
-    # Set range for BLACK color
-    black_lower = np.array([0, 0, 0])
-    black_upper = np.array([255,255, 30])
 
     red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
     green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
     white_mask = cv2.inRange(hsvFrame, white_lower, white_upper)
-    black_mask = cv2.inRange(hsvFrame, black_lower, black_upper)
 
     if askedColor == "Red":
         mask = red_mask
@@ -30,9 +26,6 @@ def defineParams(hsvFrame, askedColor):
 
     elif askedColor == "Green":
         mask = green_mask
-
-    elif askedColor == "Black":
-        mask = black_mask
 
     return mask
 
@@ -61,7 +54,7 @@ def findColorPosInList(color):
     return None
 
 
-def drawBoundingRect(img, box, textInfo, moveX = 0, moveY = 0):
+def drawBoundingRect(img, box, textInfo):
     # parse box
     x = box[0] + moveX
     y = box[1] + moveY
@@ -71,11 +64,10 @@ def drawBoundingRect(img, box, textInfo, moveX = 0, moveY = 0):
     # parse info
     text = textInfo[0]
     colorName = textInfo[1]
-    try:
-        color = colorsNum[findColorPosInList(colorName)]
-    except:
-        color = colorsNum[0]
-        print("Picked color!")
+    color = colorsNum[findColorPosInList(colorName)]
+    if color is None:
+        print("Picked black color!")
+        color = colors[3]
     #draw rectangle
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
     #draw text
@@ -101,21 +93,31 @@ def setRegionOfInterest(choice, frame):
         # extract region of interest
         roi = frame[400:700, 0:width]  # full scene
     elif choice == 2:
-        moveY = 357
-        moveX = 680
-        roi = frame[moveY:moveY+344, moveX:moveX+547]
+        moveY = 230
+        moveX = 650
+        roi = frame[moveY:moveY+120, moveX:moveX+150]
     return roi, moveX, moveY
 
 
-def isThisColorRight(img):
-    #TODO process masked image to determine
-#    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.imshow("test", img)
+def isThisColorRight(img, expectedArea):
+
+    #imgt = cv2.GaussianBlur(img,)
+
+    cannyy = cv2.Canny(img, 125, 175)
+    dilated = cv2.dilate(cannyy, (7, 7), iterations=3)
+    #cv2.imshow("D",dilated)
+    #cv2.waitKey(10000)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > expectedArea/2:
+            return True
+   # cv2.imshow("test", cannyy)
    # key = cv2.waitKey(2000)
-    return True
+    return False
 
 
-def determineColorOfObject(img, roi):
+def determineColorOfObject(img, roi, area):
     trashHold = 10
 
     if roi[1] - trashHold > 0:
@@ -131,9 +133,9 @@ def determineColorOfObject(img, roi):
     w2 = roi[0] + roi[2] + trashHold
 
     region = img[h1:h2, w1:w2]
-    for currentColor in colors:
+    for currentColor in colors[:-1]:
         masked = maskColor(region, currentColor)
-        if isThisColorRight(masked):
+        if isThisColorRight(masked, area):
             return currentColor
     return None
 
@@ -148,25 +150,77 @@ def findContours(img, original):
     for cnt in contours:
         # calc are and remove small elements
         area = cv2.contourArea(cnt)
-
-        if area > 4000:
+        if area > 200:
             x, y, w, h = cv2.boundingRect(cnt)
             contour = (x, y, w, h)
-            color = determineColorOfObject(original, contour)
-            if area > 12000:
+            color = determineColorOfObject(original, contour, area)
+            if area > 1000:
                 type = "Big"
             else:
                 type = "Small"
-            if color is not None:
-                contoursList.append(contour)
-                typesAndColors.append((type, color))
+            if color is None:
+                color = "Black"
+            contoursList.append(contour)
+            typesAndColors.append((type, color))
     return contoursList, typesAndColors
 
-### START OF CODE
+def initTracker(box, info):
+    colorName = info[1]
+    nboxX = box[0] + moveX
+    nboxY = box[1] + moveY
+    nbox = (nboxX, nboxY, box[2], box[3])
+    if colorName == "White":
+        tracker.init(frame, nbox)
+        print("Tracker initiated")
+        ninfo = ("Tracked car", "Black")
+        bool = False
+    else:
+        bool = True
+        ninfo = info
+    return bool, nbox, ninfo
 
-colorsNum = [(250, 250, 250), (10, 10, 10), (0, 255, 0), (0, 0, 255)]
-colors = ["White", "Red", "Black", "Green"]
-colors = ["Black", "White", "Red", "Green"]
+
+def updateTrackerStatus():
+    logical, box = tracker.update(frame)
+    if logical:
+        if isRectOutOfFrame(box) or isTrackerStacked(box):
+            return True, (0, 0, 0, 0)
+    return not logical, box
+
+
+def isRectOutOfFrame(box):
+    startX = box[0]
+    startY = box[1]
+    endX = box[0] + box[2]
+    endY = box[1] + box[3]
+    dimensions = frame.shape
+    if startX > dimensions[1] | endX > dimensions[1] | startX < 0 | endX < 0:   #check X
+        print("Out of frame in X")
+        return True
+    if startY > dimensions[0] | endY > dimensions[0] | startY < 0 | endY < 0:   #check Y
+        print("Out of frame in Y")
+        return True
+    return False
+
+
+def isTrackerStacked(box):
+    global stacked_counter
+    if box == box_history:
+        stacked_counter = stacked_counter + 1
+        if stacked_counter > 5:
+            print("Tracker stacked!")
+            stacked_counter = 0
+            return True
+    return False
+
+### START OF CODE
+tracker = cv2.TrackerCSRT.create()
+isTrackerFree = True
+box_history = (0, 0, 0, 0)
+stacked_counter = 0
+
+colorsNum = [(250, 250, 250), (0, 255, 0), (0, 0, 255), (10, 10, 10)]
+colors = ["White", "Red", "Green", "Black"]
 videos = ['clasic.MOV', 'autobus.mp4', 'rec_Trim.mp4']
 choice = 2
 cap = cv2.VideoCapture(videos[choice])
@@ -176,20 +230,36 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-    red = maskColor(frame, "White")
-    cv2.imshow("Red", red)
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(10000)
+    #b = cv2.selectROI("a", frame)
+    #colormask = maskColor(frame, colors[1])
+    #cv2.imshow("r",colormask)
+
+    #key = cv2.waitKey(10000)
     if ignoreFirst > 3:
-        roi, moveX, moveY = setRegionOfInterest(choice, frame)
-        mask = object_detector.apply(roi)
-        _, mask = cv2.threshold(mask, 120, 255, cv2.THRESH_BINARY)
-        contours, contourInfo = findContours(mask, roi)
+        roi, moveX, moveY = setRegionOfInterest(choice, frame)                  # set region of interest + remember move in X and Y
+        mask = object_detector.apply(roi)                                       # apply object detector to ROI
+        _, mask = cv2.threshold(mask, 120, 255, cv2.THRESH_BINARY)              # treshold unwanted objects
+        #cv2.imshow("maska", mask)
+        contours, contourInfo = findContours(mask, roi)                         # find contours in ROI
+
         if len(contours) != len(contourInfo):
             print("Not good")
+
         for i in range(0, len(contours)):
-            drawBoundingRect(frame, contours[i], contourInfo[i], moveX, moveY)
-        cv2.imshow("roi", frame)
+            drawBoundingRect(frame, contours[i], contourInfo[i])                # bound found moving objects
+            if isTrackerFree:
+                isTrackerFree, box, info = initTracker(contours[i], contourInfo[i]) # if tracker is free, try to find right object to track
+
+        if not isTrackerFree:
+            isTrackerFree, box = updateTrackerStatus()
+            listbox = list(box)
+            box_history = tuple(listbox)
+            listbox[0] = box[0] - moveX
+            listbox[1] = box[1] - moveY
+            box = tuple(listbox)
+            drawBoundingRect(frame, box, info)
+        cv2.imshow("roi", roi)
+        cv2.imshow("Frame", frame)
     ignoreFirst = ignoreFirst + 1
 
 #    print(color)
